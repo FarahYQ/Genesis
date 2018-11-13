31376e4beb6672524fbde458fdaec31cdd276920
Farah Quader <fyquader@gmail.com>
2018-11-12 23:55:39 -0800

    Edit todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,12 @@
 require 'pry'
 class TodosController < ApplicationController
   def index
-
+    @user = User.find(params[:test])
+    @todos = @user.todos
   end
 
   def new
+    @todo = Todo.new(todo_params)
   end
 
   def edit
